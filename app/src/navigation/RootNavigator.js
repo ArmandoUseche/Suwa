2328a639f24cd20c117eb0314cd5597075bc289b
Jose <josemanuelossa26@gmail.com>
2026-08-07 17:04:28 -0500

    feat: conectar MainTabNavigator como destino tras el login/registro

--- app/src/navigation/RootNavigator.js
+++ app/src/navigation/RootNavigator.js
@@ -6,7 +6,7 @@ import WelcomeScreen from '../screens/WelcomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import RegisterSuccessScreen from '../screens/RegisterSuccessScreen';
-import MainPlaceholderScreen from '../screens/MainPlaceholderScreen';
+import MainTabNavigator from './MainTabNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -26,7 +26,7 @@ export default function RootNavigator() {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="RegisterSuccess" component={RegisterSuccessScreen} />
-      <Stack.Screen name="Main" component={MainPlaceholderScreen} />
+      <Stack.Screen name="Main" component={MainTabNavigator} />
     </Stack.Navigator>
   );
 }
